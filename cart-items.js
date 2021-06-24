953bdfd069f0a09d5fd8269f4fc8d6dc102c520b
Morgan Haskins <morgan.haskins@fusionary.com>
2021-06-23 20:28:12 -0400

    Progress so far

--- cart-items.js
+++ cart-items.js
@@ -37,15 +37,34 @@ let cartItems = [
 cart.get("/", (req, res) => {
     let cart = req.query["cartItems"]
     console.log("");
-    res.json(cartItems);
+    let maxPrice = req.query.maxPrice;
+    let returnCart = cartItems;
+    if (maxPrice){
+      returnCart = cartItems.filter((item)=> item.price.toLowerCase() === item.maxPrice.toLowerCase())
+    }
+    let prefix = req.query.prefix;
+    if(prefix){
+      returnCart = cartItems.filter((item)=> item.product.toLowerCase() === item.prefix.toLowerCase())
+    }
+    let pageSize = req.query.pageSize;
+    if(pageSize){
+      returnCart = cartItems.slice(0, pageSize);
+      
+    }
+    res.json(returnCart);
   });
 
   cart.get("/:id", (req, res) => {
-      let id = req.params.id;
+    let id = req.params.id;
     console.log("Getting ", id);
     let found = cartItems.find((item) => {
         return item.id == id
     })
+    if(found){
+
+    }else{
+      return res.status(404).send('ID Not Found');
+    }
     res.json(found);
   });
 
@@ -53,17 +72,42 @@ cart.get("/", (req, res) => {
       console.log(req.body);
       let test = req.body.test
       console.log("Test results:", test )
+      let x = cartItems.length + 1;
+      // let newItems = {
+      //   id: x,
+      //   product:
+      // }
     res.json("Adding new item..");
   });
   
 
-  cart.put("/", (req, res) => {
-    res.json("Updating the cart..");
+  cart.put("/:id", (req, res) => {
+    let id = req.params.id;
+    let found = cartitems.find((item) => item.id === id);
+    if(found){
+
+    }else{
+
+    }
+    let updatedCart = req.body;
+    cartItems[req.params.id] = {...cartItems[req.params.id], ...updatedCart};
+    res.json(cartItems[req.params.id]);
+
+
+    //id is not an index number in this case
+    //find index that matches id passed in
+    //use index to do something like this {...cartItems[req.params.id], ...updatedCart}
+    //create logic to find the id
+
   });
 
-  cart.delete("/", (req, res) => {
+  cart.delete("/:id", (req, res) => {
       //logic to delete a student
-    res.json("Deleting cart item..");
+      let id = req.params.id;
+      let index = cartItems.findIndex(item => item.id == id);
+      cartItems.splice(index, 1);
+      console.log(cartItems)
+      res.status(204).json(cartItems);
   });
 
   module.exports = cart;
